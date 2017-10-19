95969d6292ff6ef356b1f8002f1cd4ddd053172f
Natasha Robarge <30872051+natasha-robarge@users.noreply.github.com>
2017-10-19 16:03:11 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -4,23 +4,40 @@ var petfinder = require('petfinder')('a839717f845b6ee822240f22a2b5a84c', '6debdb
 const ejs = require('ejs');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
-var PORTS = 3000 || 'https://pupfetchrs.herokuapp.com/';
+mongoose.connect(process.env.MONGODB_URI || process.env.DB_CONN);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
 //ejs setup
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 //app routes
 const homeRoute = require('./routes/pages');
+const userRoute = require('./routes/users');
 
 app.get('/', homeRoute.getHomePage);
-app.get('/search', homeRoute.getDogs);
-// app.get('/')
+//app.get('/search', homeRoute.getSearchPage);
+
+app.get('/signup', homeRoute.getSignUpPage);
+
+app.post('/signup', userRoute.createUser);
+app.get('/signup/:id', userRoute.getOneUser);
+
+
+//Get dogs by zip code and grab cart page
+app.get('/cart', homeRoute.getCartPage);
+app.get('/search/:zip', homeRoute.getDogs);
+
 
 
 //app start
 
-app.listen(PORTS, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log(`Listening on Port 3000`);
 })
